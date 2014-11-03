b0962d8869488d3ff0375b995bb30ba6cb1684ab
Peter Mösenthin <peter.moesenthin@gmail.com>
2014-11-03 16:34:56 +0100

    Added more (possibly) useful events

--- OpenStreetMap/OpenStreetMap.php
+++ OpenStreetMap/OpenStreetMap.php
@@ -36,7 +36,12 @@ class OpenStreetMapPlugin extends MantisPlugin {
 	function hooks() {
 		$t_hooks = array(
 			'EVENT_VIEW_BUG_EXTRA'  => 'event_view_bug_extra',
-			'EVENT_LAYOUT_RESOURCES' => 'event_layout_resources'
+			'EVENT_LAYOUT_RESOURCES' => 'event_layout_resources',
+			'EVENT_FILTER_FIELDS' => 'event_filter_fields',
+			'EVENT_FILTER_COLUMNS' => 'event_filter_columns',
+			'EVENT_REPORT_BUG_DATA' => 'event_report_bug_data',
+			'EVENT_UPDATE_BUG_FORM' => 'event_update_bug_form',
+			'EVENT_UPDATE_BUG' => 'event_update_bug'
 		);
 		return array_merge(parent::hooks(), $t_hooks);
 	}
@@ -47,7 +52,7 @@ class OpenStreetMapPlugin extends MantisPlugin {
 	 *
 	 * Any output here should be contained within its own <table> element.
 	 *
-	 * Parameters <Integer> BugID
+	 * Parameters: <Integer> BugID
 	 *
 	 */
 	function event_view_bug_extra($p_bug_id){
@@ -61,7 +66,7 @@ class OpenStreetMapPlugin extends MantisPlugin {
    * CSS, Javascript, RSS, or any other similary resources. Note that this event is signaled after all
    * other CSS and Javascript resources are linked by MantisBT.
 	 *
-	 * return <String> HTML code to output.
+	 * Return: <String> HTML code to output.
 	 *
    */
 	function event_layout_resources(){
@@ -69,6 +74,76 @@ class OpenStreetMapPlugin extends MantisPlugin {
 		$t_html .= '<script src="http://openlayers.org/en/v3.0.0/build/ol.js" type="text/javascript"></script>';
 		return $t_html;
 	}
+
+	/**
+	 * This event allows a plugin to register custom filter objects (based on the MantisFilter
+   * class) that will allow the user to search for issues based on custom criteria or datasets. The
+   * plugin must ensure that the filter class has been defined before returning the class name for
+   * this event.
+	 *
+	 * Return: <Array> Array of class names for custom filters
+	 *
+	 */
+	function event_filter_fields(){
+		//TODO check if is useful
+	}
+
+	/**
+	 * This event allows a plugin to register custom column objects (based on the MantisColumn
+   * class) that will allow the user to view data for issues based on custom datasets. The plugin
+   * must ensure that the column class has been defined before returning the class name for this
+   * event.
+	 *
+	 * Return: <Array> Array of class names for custom columns
+	 *
+	 */
+	function event_filter_columns(){
+		//TODO check if is useful
+	}
+
+	/**
+	 * This event allows plugins to perform pre-processing of the new bug data structure after
+   * being reported from the user, but before the data is saved to the database. At this point, the
+   * issue ID is not yet known, as the data has not yet been persisted.
+	 *
+	 * Parameters: <Complex> Bug data structure (see core/bug_api.php)
+	 *
+	 * Return: <Complex> Bug data structure
+	 */
+	function event_report_bug_data($bug_data_structure){
+		//TODO implement
+	}
+
+
+  /**
+   * This event allows plugins to do processing or display form elements on the Update Issue
+   * page. It is triggered immediately before the summary text field.
+   *
+   * Parameters: <Integer> BugID
+   *
+   */
+	function event_update_bug_form(){
+		//TODO implement
+	}
+
+
+	/**
+	 * This event allows plugins to perform both pre- and post-processing of the updated bug
+   * data structure after being modified by the user, but before being saved to the database.
+	 *
+	 * Parameters: <Complex> Bug data structure (see core/bug_api.php)
+	 * 						 <Integer> BugID
+	 *
+	 * Return: <Complex> Bug data structure
+	 */
+	function event_update_bug($bug_data_structure, $bug_id){
+		//TODO implement
+	}
+
+
+
+
+
 
 }
 ?>
